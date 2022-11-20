6d0a0c232d61f77e8ca838009f3cfc0349664e96
Ilya Brant <detroitib31@gmail.com>
2022-11-21 05:46:15 +0600

    Написал вычисления

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,12 @@ fun main(args: Array<String>) {
 
     print("Введите число: ")
     var a = reader.nextInt()
+    var m = 0
+
+    while (a > 0) {
+        if (a%10 > m)
+        m = a % 10
+        a /= 10
+    }
+    print("Наибольшая цифра: $m")
 }
